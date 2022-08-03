5fb342343123c5492f0cadf69875eca5edb28b50
BrunoNishimura <by.nishimura@gmail.com>
2022-08-02 23:52:35 -0300

    feat: Aula 3 - Criaçào da ViewModel e do Controller

---
+++ AgendaTarefas/AgendaTarefas/TarefasController.cs
@@ -0,0 +1,43 @@
+﻿//Aula3: Controller e Datas
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using AgendaTarefas.Models;
+
+namespace AgendaTarefas.AgendaTarefas
+{
+    public class TarefasController : Controller
+    {
+        private readonly Contexto _contexto;
+        
+        public TarefasController(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+        public IActionResult Index()
+        {
+            return View(PegarDatas());
+        }
+
+        private List<DatasViewModel> PegarDatas()
+        {
+            DateTime dataAtual = DateTime.Now;
+            DateTime dataLimite = DateTime.Now.AddDays(10);
+            int qtdDias = 0;
+            DatasViewModel data;
+            List<DatasViewModel> listaDatas = new List<DatasViewModel>();
+
+            while(dataAtual < dataLimite)
+            {
+                data = new DatasViewModel();
+                data.Datas = dataAtual.ToShortDateString();
+                data.Identificadores = "collapse" + dataAtual.ToShortDateString().Replace("/", "");
+                listaDatas.Add(data);
+                qtdDias = qtdDias + 1;
+                dataAtual = dataAtual.AddDays(qtdDias);
+            }
+
+            return listaDatas;
+        }
+    }
+}
---
+++ AgendaTarefas/Models/DatasViewModel.cs
@@ -0,0 +1,12 @@
+﻿//Aula3: Criação de uma ViewModel para nos auxiliar na manipulação das Datas.
+
+namespace AgendaTarefas.Models
+{
+	public class DatasViewModel
+	{
+		public string? Datas { get; set; }
+
+		public string? Identificadores { get; set; }
+
+	}
+}
