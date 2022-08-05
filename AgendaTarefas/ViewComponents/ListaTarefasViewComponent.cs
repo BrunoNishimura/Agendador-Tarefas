25bfe6e65daffec11b4f91349378ae6f5a0ca562
BrunoNishimura <by.nishimura@gmail.com>
2022-08-04 23:05:38 -0300

    feat: Aula 5 - Created a ViewComponent

---
+++ AgendaTarefas/ViewComponents/ListaTarefasViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using AgendaTarefas.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgendaTarefas.ViewComponents
+{
+    public class ListaTarefasViewComponent : ViewComponent
+    {
+        private readonly Contexto _contexto;
+
+        //Criando o Construtor e inicializa-lo via injeção de dependências.
+        public ListaTarefasViewComponent(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        //IViewComponent é um método obrigatório.
+        public async Task<IViewComponentResult> InvokeAsync(string data)
+        {
+            return View(await _contexto.Tarefas.OrderBy(t => t.Horario).Where(t => t.Data == data).ToListAsync());
+        }
+
+    }
+}
